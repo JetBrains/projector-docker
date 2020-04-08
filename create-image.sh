cf45839f38cf1da382d491df35499e1cda397429
SerVB <servbul@yandex.ru>
2020-04-08 18:16:05 +0300

    Make default archive name more logical

--- create-image.sh
+++ create-image.sh
@@ -4,6 +4,6 @@ set -e # Any command which returns non-zero exit code will cause this shell scri
 set -x # Activate debugging to show execution details: all commands will be printed before execution
 
 containerName=${1:-projector-idea-c}
-tarGzFileName=${2:-projector-idea-c.tar.gz}
+tarGzFileName=${2:-$containerName.tar.gz}
 
 docker save "$containerName" | gzip > "$tarGzFileName"
