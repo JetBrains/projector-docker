f5a33787d13abe5d8b20a3099195dee3d3288811
SerVB <servbul@yandex.ru>
2020-04-08 16:38:13 +0300

    Move MD plugin disabling to starter script to support mount dir

---
+++ static/enable-md-plugin.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e # Any command which returns non-zero exit code will cause this shell script to exit immediately
+
+configDirRegex='VM_OPTIONS_FILE="(.*\$HOME.*)\/.*\.vmoptions"'
+
+configDirCandidates=($(grep -ohrE --include=*.sh "$configDirRegex" /projector/ide/bin))
+
+# map candidates to the matches:
+for i in "${!configDirCandidates[@]}"; do
+    if [[ ${configDirCandidates[i]} =~ $configDirRegex ]]; then
+        configDirCandidates[i]="${BASH_REMATCH[1]}"
+    else
+        unset 'configDirCandidates[i]'
+    fi
+done
+
+if [[ ${#configDirCandidates[@]} != 1 ]]; then
+    echo "Can't find a single candidate to be IDE config dir so can't select a single one:"
+    echo "${configDirCandidates[*]}"
+    exit 1
+fi
+
+configDir=${configDirCandidates[@]}
+
+echo "Found IDE config dir: $configDir"
+
+mdPluginEnablerFile="/projector/ide/bin/mdPluginEnablerFile.sh"
+touch "$mdPluginEnablerFile"
+chmod +x "$mdPluginEnablerFile"
+echo "mkdir -p \"$configDir\"" >> "$mdPluginEnablerFile"
+echo "cp /projector/disabled_plugins.txt $configDir/" >> "$mdPluginEnablerFile"
+."$mdPluginEnablerFile"
--- static/run.sh
+++ static/run.sh
@@ -3,6 +3,8 @@
 set -e # Any command which returns non-zero exit code will cause this shell script to exit immediately
 set -x # Activate debugging to show execution details: all commands will be printed before execution
 
+./projector/enable-md-plugin.sh
+
 cd /projector/ide/bin
 
 ./ide-projector-launcher.sh
