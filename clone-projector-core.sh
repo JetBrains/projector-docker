c9b61a37fc787c917d60d1b53e75cd173f053fef
SerVB <servbul@yandex.ru>
2020-11-03 22:45:47 +0300

    PRJ-128 Remove cloning client project since client files are now bundled to server

--- clone-projector-core.sh
+++ clone-projector-core.sh
@@ -19,5 +19,4 @@
 set -e # Any command which returns non-zero exit code will cause this shell script to exit immediately
 set -x # Activate debugging to show execution details: all commands will be printed before execution
 
-git clone https://github.com/JetBrains/projector-client.git ../projector-client
 git clone https://github.com/JetBrains/projector-server.git ../projector-server
